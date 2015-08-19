6b230eb3bbfa6aceb411dd3d3ed7bc83996673c4
mzdunek93 <mzdunek93@gmail.com>
2015-08-19 20:57:24 +0200

    Get student decorator specs to pass

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,7 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_item_notes.where(subject_item: subject_item)
+    "%.2f" % (notes.length > 0 ? notes.sum(:value) / notes.length.to_f : 0)
   end
 end
